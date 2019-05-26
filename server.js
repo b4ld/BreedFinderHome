288e5ed8d270f5cfed7486729a99a1331b5fe53d
Pedro Carmezim <self.pecar@gmail.com>
2019-05-27 00:50:32 +0100

    add html sendFile

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const app = express()
 const fs = require('fs')
+var path = require('path');
+
 
 const express = require('express')
 
 app.use(express.urlencoded())
+
+const port = 3040
+
+
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
 
 app.post('/submit-form', (req, res) => {
     const email = req.body.username
     
     fs.appendFile('emailList.txt', email, (err) => {
         if (err) {
-          console.error(err)
-          return
+            console.error(err)
+            return
         }
         console.log("SAVED")
-      })
-
+    })
+    
     res.end()
-  })
+})
+
+
+
+app.listen(port, () => console.log('App listening on port ${port}!'))
